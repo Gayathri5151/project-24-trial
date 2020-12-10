f176e12a991f6323e6050be3ec3f31045b93273a
Gayathri5151 <73778830+Gayathri5151@users.noreply.github.com>
2020-12-10 10:13:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-24-trial
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var Paper
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+    
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  keyPressed();
+  
+  drawSprites();
+}
+
+function keyPressed()
+{
+  if(keyCode===UP_ARROW)
+  {
+    Matter.Body.applyForce(paperObject,paperObject.body.position,{x:85,y:85})
+  }
+}
+
+
+
+
+
